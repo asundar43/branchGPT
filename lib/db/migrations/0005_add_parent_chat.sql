7c7bdd7fc49ae175a7ed96b3f055799547eaed0b
asundar43 <asundar43@gatech.edu>
2025-03-17 15:40:04 -0400

    fixed db migration conflicts

--- lib/db/migrations/0005_add_parent_chat.sql
+++ lib/db/migrations/0005_add_parent_chat.sql
@@ -1,4 +1,8 @@
-ALTER TABLE "Chat" ADD COLUMN "parentId" varchar(36);
+DO $$ BEGIN
+ ALTER TABLE "Chat" ADD COLUMN "parentId" varchar(36);
+EXCEPTION
+ WHEN duplicate_column THEN null;
+END $$;
 
 DO $$ BEGIN
  ALTER TABLE "Chat" ADD CONSTRAINT "Chat_parentId_Chat_id_fk" FOREIGN KEY ("parentId") REFERENCES "public"."Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
