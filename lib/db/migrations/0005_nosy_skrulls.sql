1107a5a005d10bcda3551ce913f649c55cb687e3
asundar43 <asundar43@gatech.edu>
2025-03-17 16:20:52 -0400

    fixed db migration conflicts

--- lib/db/migrations/0005_nosy_skrulls.sql
+++ lib/db/migrations/0005_nosy_skrulls.sql
@@ -1,8 +1,4 @@
-DO $$ BEGIN
- ALTER TABLE "Chat" ADD COLUMN "parentId" varchar(36);
-EXCEPTION
- WHEN duplicate_column THEN null;
-END $$;
+ALTER TABLE "Chat" ADD COLUMN "parentId" varchar(36);
 
 DO $$ BEGIN
  ALTER TABLE "Chat" ADD CONSTRAINT "Chat_parentId_Chat_id_fk" FOREIGN KEY ("parentId") REFERENCES "public"."Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- lib/db/migrations/0009_odd_ben_parker.sql
+++ lib/db/migrations/0009_odd_ben_parker.sql
@@ -13,12 +13,20 @@ ALTER TABLE "Chat" DROP CONSTRAINT "Chat_branchFromMessageId_Message_id_fk";
 --> statement-breakpoint
 ALTER TABLE "Chat" DROP CONSTRAINT "Chat_branchStartMessageId_Message_id_fk";
 --> statement-breakpoint
-ALTER TABLE "Chat" ADD COLUMN "parentId" varchar(36);--> statement-breakpoint
-ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_mainChatId_Chat_id_fk" FOREIGN KEY ("mainChatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_branchedChatId_Chat_id_fk" FOREIGN KEY ("branchedChatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_mainMessageId_Message_id_fk" FOREIGN KEY ("mainMessageId") REFERENCES "public"."Message"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_branchMessageId_Message_id_fk" FOREIGN KEY ("branchMessageId") REFERENCES "public"."Message"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE UNIQUE INDEX "unq_branched_chat" ON "ChatBranches" USING btree ("branchedChatId");--> statement-breakpoint
-ALTER TABLE "Chat" DROP COLUMN "branchFromChatId";--> statement-breakpoint
-ALTER TABLE "Chat" DROP COLUMN "branchFromMessageId";--> statement-breakpoint
+ALTER TABLE "Chat" ALTER COLUMN "parentId" SET DATA TYPE varchar(36);
+--> statement-breakpoint
+ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_mainChatId_Chat_id_fk" FOREIGN KEY ("mainChatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_branchedChatId_Chat_id_fk" FOREIGN KEY ("branchedChatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_mainMessageId_Message_id_fk" FOREIGN KEY ("mainMessageId") REFERENCES "public"."Message"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+ALTER TABLE "ChatBranches" ADD CONSTRAINT "ChatBranches_branchMessageId_Message_id_fk" FOREIGN KEY ("branchMessageId") REFERENCES "public"."Message"("id") ON DELETE no action ON UPDATE no action;
+--> statement-breakpoint
+CREATE UNIQUE INDEX "unq_branched_chat" ON "ChatBranches" USING btree ("branchedChatId");
+--> statement-breakpoint
+ALTER TABLE "Chat" DROP COLUMN "branchFromChatId";
+--> statement-breakpoint
+ALTER TABLE "Chat" DROP COLUMN "branchFromMessageId";
+--> statement-breakpoint
 ALTER TABLE "Chat" DROP COLUMN "branchStartMessageId";
